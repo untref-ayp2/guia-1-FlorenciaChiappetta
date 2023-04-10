84154c7ac1936a474d5428f41f8c9da9d3f1c1ec
FlorenciaChiappetta <111155028+FlorenciaChiappetta@users.noreply.github.com>
2023-04-09 23:53:28 -0300

    comit

--- 00-mediciones/main/main.go
+++ 00-mediciones/main/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-	"busquedas"
 	"fmt"
-	"sort"
-	"time"
-	"utiles"
 )
 
+func Burbuja(ListaDesordenada []int) []int {
+	var auxiliar int
+	for i := 0; i < len(ListaDesordenada); i++ {
+		for j := 0; j < len(ListaDesordenada); j++ {
+			if ListaDesordenada[i] > ListaDesordenada[j] {
+				auxiliar = ListaDesordenada[i]
+				ListaDesordenada[i] = ListaDesordenada[j]
+				ListaDesordenada[j] = auxiliar
+			}
+		}
+	}
+	return ListaDesordenada
+}
 func main() {
-	arreglo := utiles.GenerarArreglo(10, 100000)
+	/*arreglo := utiles.GenerarArreglo(10, 1000)
 	buscado := -1
 
 	//fmt.Println(arreglo)
@@ -28,6 +37,9 @@ func main() {
 	inicio = time.Now()
 	// Busqueda Binaria
 	fmt.Println(busquedas.BusquedaBinaria(arreglo, buscado))
-	fmt.Println("Busqueda Binaria: ", time.Since(inicio))
+	fmt.Println("Busqueda Binaria: ", time.Since(inicio))*/
 
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	Burbuja(numbers)
+	fmt.Println(numbers)
 }
