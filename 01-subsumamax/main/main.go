2288d2edb8af9fab3e0c10d6bcb9659b6cacc68b
FlorenciaChiappetta <111155028+FlorenciaChiappetta@users.noreply.github.com>
2023-05-07 11:23:07 -0300

    Update main.go

--- 01-subsumamax/main/main.go
+++ 01-subsumamax/main/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	arreglo := []int{-1, 6, -2, 5, -1, 4, 3, -4, 3}
+	//arreglo := []int{-1, 6, -2, 5, -1, 4, 3, -4, 3}
+	arreglo := []int{-2, 11, -4, 2, -3, -10}
 	//sumaMaxima, posInicial, posFinal := subsumamax.SubsecuenciaSumaMaxima(arreglo)
 	//fmt.Printf("La subsecuencia con suma máxima es: %v\n", arreglo[posInicial:posFinal+1])
 	//fmt.Printf("Punto Critico en función: %v\n", sumaMaxima)
@@ -14,5 +15,13 @@ func main() {
 	//fmt.Printf("La posición final es: %v\n", posFinal)
 	sumaMaxima, sumaDerechaAIzquierda, sumaIzquierdaADerecha, sumaCentral := subsumamax.SubsecuenciaSumaMaxima2(arreglo)
 	fmt.Printf("La suma máxima es: %v\n,%v\n,%v\n,%v\n", sumaMaxima, sumaDerechaAIzquierda, sumaIzquierdaADerecha, sumaCentral)
+	fmt.Printf("   *   \n")
+	fmt.Printf("  * *  \n")
+	fmt.Printf(" * * * \n")
+	fmt.Printf("* * * * \n")
 
+	b := "PRUEBA"
+	ASCII := []byte(b)
+
+	fmt.Println(ASCII)
 }
